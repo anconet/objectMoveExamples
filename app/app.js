7d22419b2326d292fe1d39739dfae00312c9da55
Anthony Anconetani <anthony.anconetani@gmail.com>
2024-01-14 11:46:49 -0600

    saving the code

--- app/app.js
+++ app/app.js
@@ -1,7 +1,26 @@
 
-import React from 'react'
+import { useState } from 'react';
+import { useWindowListener } from './useWindowListener.js';
 
 export default function App() {
-    return <div>Hello World!</div>
+    const [position, setPosition] = useState({ x: 0, y: 0 });
 
+    useWindowListener('pointermove', (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+    });
+
+    return (
+        <div style={{
+            position: 'absolute',
+            backgroundColor: 'pink',
+            borderRadius: '50%',
+            opacity: 0.6,
+            transform: `translate(${position.x}px, ${position.y}px)`,
+            pointerEvents: 'none',
+            left: -20,
+            top: -20,
+            width: 40,
+            height: 40,
+        }} />
+    );
 }
